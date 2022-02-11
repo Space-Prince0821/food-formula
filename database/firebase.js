a263a1ff1e9b521c0430d445fafa347f54bf1992
Brandon <brandon.tong.581@my.csun.edu>
2022-02-11 13:07:54 -0800

    fixed a errors, firebase module problem persists

--- database/firebase.js
+++ database/firebase.js
@@ -1,5 +1,7 @@
 //database
-import * as database from 'firebase';
+import * as firebase from 'firebase';
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyANFqv0H50VcB1IRUzvpPuWJZYSFoVUbT8",
@@ -11,4 +13,6 @@ const firebaseConfig = {
     measurementId: "G-F6DHJWB9LF"
 };
 
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export default firebase;
