caf3aaaa62a9c4ac0f553076b2984399ad043cb4
Brandon <brandon.tong.581@my.csun.edu>
2022-02-15 13:46:01 -0800

    changes to login

--- components/login.js
+++ components/login.js
@@ -35,8 +35,79 @@ export default class Login extends Component {
           email: '',
           password: '',
         })
-        this.props.navigation()
+        this.props.navigate('Home')
       })
+      .catch(error => this.setState({errorMessage: error.errorMessage}))
     }
   }
+  render() {
+    if(this.state.isLoading) {
+      return(
+        <View style={styles.preloader}>
+          <ActivityIndicatior size="large" color="#9E9E9E"/>
+        </View>
+      )
+    }
+    return (
+      <View style={styles.container}>  
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="Email"
+          value={this.state.email}
+          onChangeText={(val) => this.updateInputVal(val, 'email')}
+        />
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="Password"
+          value={this.state.password}
+          onChangeText={(val) => this.updateInputVal(val, 'password')}
+          maxLength={15}
+          secureTextEntry={true}
+        />   
+        <Button
+          color="#3740FE"
+          title="Register"
+          onPress={() => this.userLogin()}
+        />   
+        <Text 
+          style={styles.loginText}
+          onPress={() => this.props.navigation.navigate('Signup')}>
+          Click here to signup
+        </Text>                          
+      </View>
+    );
+  }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 35,
+    backgroundColor: '#fff'
+  },
+  inputStyle: {
+    width: '100%',
+    marginBottom: 15,
+    paddingBottom: 15,
+    alignSelf: "center",
+    borderColor: "#ccc",
+    borderBottomWidth: 1
+  },
+  loginText: {
+    color: '#3740FE',
+    marginTop: 25,
+    textAlign: 'center'
+  },
+  preloader: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  }
+});
