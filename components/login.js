0826d8337554e560c39553b53231033647fb196d
Brandon <brandon.tong.581@my.csun.edu>
2022-02-15 13:37:58 -0800

    changes to reg/login

--- components/login.js
+++ components/login.js
@@ -1,3 +1,42 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert, ActivityIndicatior} from 'react-native';
 import firebase from '../database/firebase';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert, ActivityIndicatior } from 'react-native';
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isLoading: false
+    }
+  }
+  updateInputVal = (val,prop) => {
+    const state = this.state;
+    state[prop] = val;
+    this.setState(state);
+  }
+  userLogin = () => {
+    if(this.state.email === '' && this.state.password === '') {
+      Alert.alert('Email or password incorrect')
+    }
+    else {
+      this.setState({
+        isLoading: true,
+      })
+      firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((res) => {
+        console.log(res)
+        console.log('User login success!')
+        this.setState({
+          isLoading: false,
+          email: '',
+          password: '',
+        })
+        this.props.navigation()
+      })
+    }
+  }
+}
--- components/register.js
+++ components/register.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, Alert, ActivityIndicatior} from 'react-native';
 import firebase from '../database/firebase';
 
-export default class Signup extends Component {
+export default class Register extends Component {
   
   constructor() {
     super();
