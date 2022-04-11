478879e7926666caf659a6454867b039a3767a94
Prince Chowdury <78129627+Space-Prince0821@users.noreply.github.com>
2022-04-11 11:26:38 -0700

    Revert "Delete screens directory"
    This reverts commit 3b95e0e172848ab5367f6ab479a3799f0688e40d.

---
+++ screens/CameraScreen.js
@@ -0,0 +1,175 @@
+import { useNavigation } from "@react-navigation/native";
+import React, { useState, useEffect} from "react";
+import { View, Text, Image, StyleSheet, Alert } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { Camera } from "expo-camera";
+import flip from '../assets/flip.jpg';
+import * as ImagePicker from 'expo-image-picker';
+import { getStorage, ref, uploadBytes, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import uuid from "uuid";
+//import * as firebase from "firebase";
+
+export default function CameraScreen() {
+    const navigation = useNavigation();
+
+    //Camera state
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+    const [imageUri, setImageUri] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === "granted");
+        }) ();
+    },  []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>Access to camera denied</Text>;
+    }
+
+    const getPicInfo = (x) => {
+        fetch(
+            'https://api.spoonacular.com/food/images/analyze?apiKey=4b70e356c2ad48e58244c333fd2693b5&imageUrl=' + x
+        )
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+        })
+        .catch(() => {
+            console.log('error')
+        });
+    }
+
+    const pictureTaken = async () => { 
+        let data = await this.camera.takePictureAsync(null);
+        console.log(data.uri);
+        setImageUri(data.uri);
+    };
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [1, 1],
+          quality: 1,
+        });
+
+        if (!result.cancelled) {
+            setImageUri(result.uri);
+            console.log(result.uri);
+            const storage = getStorage();
+            const filename = (imageUri).substring(imageUri.lastIndexOf('/') + 1);
+            const reference = ref(storage, filename);
+
+            const img = await fetch(result.uri);
+            const bytes = await img.blob();
+
+            //Issue over here
+            await uploadBytes(reference, bytes).then(() => {
+                console.log("Uploaded successfully!");
+            });
+
+            getURL(reference);
+            /*await getDownloadURL(ref).then((x) => {
+                console.log(x);
+                //.catch((err) => {console.log(err)})
+                /*fetch(
+                    'https://api.spoonacular.com/food/images/analyze?apiKey=4b70e356c2ad48e58244c333fd2693b5&imageUrl=' + x
+                )
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data)
+                })
+                .catch(() => {
+                    console.log('error')
+                });
+            }*/
+        };
+    };
+
+    const getURL = async (reference) => {
+        await getDownloadURL(reference).then((x) => {
+            console.log(x);
+            fetch(
+                'https://api.spoonacular.com/food/images/analyze?apiKey=4b70e356c2ad48e58244c333fd2693b5&imageUrl=' + x
+            )
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+            })
+            .catch(() => {
+                console.log('error')
+            });
+        });
+    };
+
+
+    return (
+        <View >
+        <Camera style={styles.camera} 
+            type={type}
+            ref={(ref) => {camera = ref}}
+            >
+                <View style={styles.buttons}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setType(
+                                type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back
+                            )
+                        }}>
+                        <Image style={styles.touchContainer} source={flip} alt={"Flip"}/>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={pictureTaken}>
+                        <Image source={{ uri: 'https://www.mcicon.com/wp-content/uploads/2021/02/Technology_Camera_1-copy-8.jpg' }} style={styles.cameraSelect} />
+                        <Text title={'Gallery'} onPress={pickImage} />
+                        <TouchableOpacity onPress={pickImage}>
+                            <Image source={{ uri: imageUri}} style={{ flex: 0.7}}/>
+                        </TouchableOpacity>
+                    </TouchableOpacity>
+                </View>
+            </Camera>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    cameraSelect: {
+        width: 60,
+        height: 60,
+        overflow: "hidden",
+        borderRadius: 150/3,
+    },
+    camera: {
+        width: 414,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 750,
+    },
+    touchContainer: {
+        padding: 15,
+        marginBottom: 10,
+        width: 60,
+        height: 60,
+        backgroundColor: "#fff",
+        borderRadius: 150/3,
+    },
+    buttons: {
+        width: 410,
+        height: 140,
+        padding: 5,
+        alignItems: 'flex-end',
+        justifyContent: 'space-between'
+    }
+});
